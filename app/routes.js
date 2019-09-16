2b5fc8c2784b259a3587661d6fd8c08967606bae
Mat Carey <mat@yagni.digital>
2019-09-16 09:43:01 +0100

    Added back buttons and checking the flow is being followed.

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,30 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.use((req, res, next) => {
-    req.session.data.formattedAddress = (req.session.data.address || '').replace(/\n/g, '<br/>')
-    next()
+router.use('/summary', (req, res, next) => {
+  req.session.data.formattedAddress = (req.session.data.address || '').replace(/\n/g, '<br/>')
+  next()
 })
+
+router.post('/confirm', (req, res) => {
+  req.session.data = {submitted: true}
+  res.redirect('/confirm')
+})
+
+const redirectIfNotSet = (from, keys, to) => {
+  router.use(from, (req, res, next) => {
+    if (keys.map(key => req.session.data[key] === undefined).includes(true)) {
+      res.redirect(to)
+    } else {
+      next()
+    }
+  })
+}
+
+redirectIfNotSet('/confirm', ['submitted'], '/summary')
+redirectIfNotSet('/summary', ['name', 'phoneNumber', 'address', 'canWeWrite'], '/can-we-write')
+redirectIfNotSet('/can-we-write', ['name', 'phoneNumber', 'address'], '/address')
+redirectIfNotSet('/address', ['name', 'phoneNumber'], '/phone-number')
+redirectIfNotSet('/phone-number', ['name'], '/name')
 
 module.exports = router
